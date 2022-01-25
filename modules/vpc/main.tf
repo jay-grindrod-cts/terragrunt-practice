fa643a38ef6eabfb99f8d312c0573113e8377775
Jay Grindrod <jay.grindrod@cts.co>
2022-01-25 08:48:39 +0000

    fixing module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,7 @@
 module "network" {
-  source  = "tfr:///terraform-google-modules/network/google?version=4.0.1"
-  
+  source  = "terraform-google-modules/network/google"
+  version = "4.1.0"
+
   project_id = var.project
   network_name = var.network_name
 }
